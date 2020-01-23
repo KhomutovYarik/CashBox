855ddd8150c1487840bd01e3b54a2cf039eaa663
Yaroslav Khomutov <homutov-yarik@mail.ru>
2020-01-24 03:43:18 +0500

    initial commit

--- app/src/main/java/com/example/cashbox/OrdersActivity.java
+++ app/src/main/java/com/example/cashbox/OrdersActivity.java
@@ -3,12 +3,37 @@ package com.example.cashbox;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class OrdersActivity extends AppCompatActivity {
+    private Button activeButton, finishedButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_orders);
+        preparing();
+    }
+
+    private void preparing()
+    {
+        activeButton = findViewById(R.id.activeOrdersButton);
+        finishedButton = findViewById(R.id.finishedOrdersButton);
+        activeButton.setBackground(getResources().getDrawable(R.drawable.focuschangedbutton));
+        activeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                activeButton.setBackground(getResources().getDrawable(R.drawable.focuschangedbutton));
+                finishedButton.setBackground(getResources().getDrawable(R.drawable.nonfocuschangedbutton));
+            }
+        });
+        finishedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                activeButton.setBackground(getResources().getDrawable(R.drawable.nonfocuschangedbutton));
+                finishedButton.setBackground(getResources().getDrawable(R.drawable.focuschangedbutton));
+            }
+        });
     }
 }
