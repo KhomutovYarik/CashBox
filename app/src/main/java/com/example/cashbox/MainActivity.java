06eda27e4fc2e9af2268a9e9266f4ab5b7a62a8b
dima059 <userdima059@gmail.com>
2020-01-23 20:18:00 +0500

    Initial commit

--- app/src/main/java/com/example/cashbox/MainActivity.java
+++ app/src/main/java/com/example/cashbox/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.cashbox;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
+import ru.tinkoff.decoro.MaskImpl;
+import ru.tinkoff.decoro.slots.PredefinedSlots;
+import ru.tinkoff.decoro.watchers.FormatWatcher;
+import ru.tinkoff.decoro.watchers.MaskFormatWatcher;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FormatWatcher formatWatcher = new MaskFormatWatcher(
+                MaskImpl.createTerminated(PredefinedSlots.RUS_PHONE_NUMBER)
+        );
+        EditText editText = findViewById(R.id.phoneNum);
+        formatWatcher.installOn(editText); // тут аргументом может быть любой TextView
+
     }
+
 }
