7064229bda46c21bdb0240535c1de5a9f1ab0af7
Yaroslav Khomutov <homutov-yarik@mail.ru>
2020-01-23 23:31:23 +0500

    initial commit

--- app/src/main/java/com/example/cashbox/MainActivity.java
+++ app/src/main/java/com/example/cashbox/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.cashbox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText phoneNumber, password;
+    private Button joinButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        preparing();
+    }
+
+    private void preparing()
+    {
+        joinButton = (Button)findViewById(R.id.joinButton);
+        joinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent orders = new Intent(MainActivity.this, OrdersActivity.class);
+                startActivity(orders);
+            }
+        });
     }
 }
