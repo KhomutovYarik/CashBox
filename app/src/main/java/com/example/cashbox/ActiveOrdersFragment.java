dbbe2e5ba0858bae16088524d14b604dd19397ff
Yaroslav <homutov-yarik@mail.ru>
2020-01-26 00:06:02 +0500

    initial commit

--- app/src/main/java/com/example/cashbox/ActiveOrdersFragment.java
+++ app/src/main/java/com/example/cashbox/ActiveOrdersFragment.java
@@ -8,16 +8,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class ActiveOrdersFragment extends Fragment {
 
+    View view;
+    ImageView removeButton;
+    ListView ordersList;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_active_orders, container, false);
+        view = inflater.inflate(R.layout.fragment_active_orders, container, false);
 
         ArrayList<ActiveOrder> activeOrdersList = new ArrayList<ActiveOrder>();
 
@@ -26,10 +31,23 @@ public class ActiveOrdersFragment extends Fragment {
         activeOrdersList.add(new ActiveOrder("#6213654. Активная заявка", "Касса №2, ", "Партнер ККМ №2",
                 "Не работает", "1 предложение", "от 3000 р"));
 
-        ListView ordersList = view.findViewById(R.id.activeOrdersList);
+        ordersList = view.findViewById(R.id.activeOrdersList);
         ActiveOrderAdapter adapter = new ActiveOrderAdapter(getActivity(), R.layout.orderslistelement, activeOrdersList);
         ordersList.setAdapter(adapter);
 
+
         return view;
+    }
+
+    private void preparing()
+    {
+        removeButton = view.findViewById(R.id.removeButton);
+
+        removeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 }
